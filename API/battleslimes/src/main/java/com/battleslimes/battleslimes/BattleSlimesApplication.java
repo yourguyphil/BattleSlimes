b53b97c1107f27a0d71f2d8a487833fba787efaf
Philips Nguyen <philips.nguyen96@gmail.com>
2021-12-27 15:15:27 -0600

    SpringBoot Starter

---
+++ API/battleslimes/src/main/java/com/battleslimes/battleslimes/BattleSlimesApplication.java
@@ -0,0 +1,22 @@
+package com.battleslimes.battleslimes;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class BattleSlimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleSlimesApplication.class, args);
+		log.info("__________         __    __  .__           _________.__  .__                       ");
+		log.info("\\______   \\_____ _/  |__/  |_|  |   ____  /   _____/|  | |__| _____   ____   ______");
+		log.info(" |    |  _/\\__  \\\\   __\\   __\\  | _/ __ \\ \\_____  \\ |  | |  |/     \\_/ __ \\ /  ___/");
+		log.info(" |    |   \\ / __ \\|  |  |  | |  |_\\  ___/ /        \\|  |_|  |  Y Y  \\  ___/ \\___ \\ ");
+		log.info(" |______  /(____  /__|  |__| |____/\\___  >_______  /|____/__|__|_|  /\\___  >____  >");
+		log.info("        \\/      \\/                     \\/        \\/               \\/     \\/     \\/");
+		log.info("BattleSlimes Service has successfully started!");
+	}
+
+}
---
+++ API/battleslimes/src/test/java/com/battleslimes/battleslimes/BattleslimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battleslimes.battleslimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleslimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
